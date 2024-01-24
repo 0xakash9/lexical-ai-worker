eebb807ea80abe8173c56524c2a588a8860c4f8b
0xakash9 <devakash256@gmail.com>
2024-01-24 15:13:16 +0530

    feat:add openai  worker

--- src/worker.js
+++ src/worker.js
@@ -1,15 +1,36 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run "npm run dev" in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run "npm run deploy" to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import OpenAI from 'openai';
 
 export default {
-  async fetch(request, env, ctx) {
-    return new Response('Hello World!');
-  },
+	async fetch(request, env, ctx) {
+		const requestBody = await request.json();
+		const secret_key = request.headers.get('secret_key');
+		const text = requestBody.text;
+		const options = requestBody.option;
+		if (!secret_key) return new Response('Invalid secret key', { status: '500' });
+		const openai = new OpenAI({
+			apiKey: secret_key,
+		});
+
+		const params = {
+			messages: [
+				{ role: 'system', content: 'You are a helpful text editor assistant.' },
+				{
+					role: 'user',
+					content: `${options} the following text from editor: ${text}`,
+				},
+			],
+			model: 'gpt-3.5-turbo',
+			temperature: 0,
+			top_p: 1,
+			frequency_penalty: 0,
+			presence_penalty: 0,
+			max_tokens: 2000,
+			n: 1,
+		};
+
+		const chatCompletion = await openai.chat.completions.create(params);
+		const openAIResponse = chatCompletion.choices[0].message;
+
+		return new Response(openAIResponse);
+	},
 };
